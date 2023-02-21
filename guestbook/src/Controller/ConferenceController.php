bb11143debf055ccc9cc3b3962a088f77ce7472e
hadnotb <adnot.hichem@gmail.com>
2023-02-21 08:52:45 +0000

    upd

--- guestbook/src/Controller/ConferenceController.php
+++ guestbook/src/Controller/ConferenceController.php
@@ -2,17 +2,37 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ConferenceController extends AbstractController
 {
-    #[Route('/conference', name: 'app_conference')]
-    public function index(): Response
+    #[Route('/', name: 'homepage')]
+    public function index(Request $req): Response
     {
-        return $this->render('conference/index.html.twig', [
-            'controller_name' => 'ConferenceController',
+        $greet ='';
+        if($name = $req->query->get('hello')){
+            $greet = sprintf('<h1>Hello %s!</h1>', htmlspecialchars($name));
+        }
+        dd($req);
+        
+        return new Response(<<<EOF
+                    <html>
+                        <body><img src="/images/under-construction.gif" /></body>
+                    </html>
+               EOF);
+    }
+
+    #[Route('/conference', name: 'conference')]
+    public function conference(Request $req): Response
+    {
+
+        $data = '';
+
+        return $this->render('conference/index.html.twig',[
+            "data" => $data
         ]);
     }
 }
